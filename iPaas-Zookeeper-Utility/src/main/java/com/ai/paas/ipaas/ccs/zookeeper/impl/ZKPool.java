c7e2952744ed50cfc6be3374ccd75db9b2bc4b09
zhang.xin <ascrutae@gmail.com>
2016-03-16 14:02:50 +0800

    Add method without zkuser and zkpasswd parameters.

--- iPaas-Zookeeper-Utility/src/main/java/com/ai/paas/ipaas/ccs/zookeeper/impl/ZKPool.java
+++ iPaas-Zookeeper-Utility/src/main/java/com/ai/paas/ipaas/ccs/zookeeper/impl/ZKPool.java
@@ -1,6 +1,7 @@
 package com.ai.paas.ipaas.ccs.zookeeper.impl;
 
 import com.ai.paas.ipaas.ccs.zookeeper.ZKClient;
+import com.ai.paas.ipaas.util.StringUtil;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,33 +11,43 @@ public class ZKPool {
     public ZKClient getZkClient(final String zkAddr, final String zkUserName) throws Exception {
         return clients.get(appendKey(zkAddr, zkUserName));
     }
-    
-    public ZKClient getZkClient(final String zkAddr, final String zkUserName ,final String serviceId) throws Exception {
-        return clients.get(appendKey(zkAddr, zkUserName,serviceId));
+
+    public ZKClient getZkClient(final String zkAddr, final String zkUserName, final String serviceId) throws Exception {
+        return clients.get(appendKey(zkAddr, zkUserName, serviceId));
     }
 
     private String appendKey(String zkAddr, String zkUserName) {
-        return zkAddr + "-" + zkUserName;
+        return appendKey(zkAddr, zkUserName, null);
     }
-    
-    private String appendKey(String zkAddr, String zkUserName,String serviceId) {
-        return zkAddr + "-" + zkUserName + "-" + serviceId;
+
+    private String appendKey(String zkAddr, String zkUserName, String serviceId) {
+        StringBuilder key = new StringBuilder(zkAddr);
+        if (!StringUtil.isBlank(zkUserName)) {
+            key.append("-");
+            key.append(zkUserName);
+        }
+
+        if (!StringUtil.isBlank(serviceId)) {
+            key.append("-");
+            key.append(serviceId);
+        }
+        return key.toString();
     }
 
     public void addZKClient(final String zkAddr, final String zkUserName, ZKClient zkClient) {
         clients.put(appendKey(zkAddr, zkUserName), zkClient);
     }
-    
-    public void addZKClient(final String zkAddr, final String zkUserName, final String serviceId ,ZKClient zkClient) {
-        clients.put(appendKey(zkAddr, zkUserName,serviceId), zkClient);
+
+    public void addZKClient(final String zkAddr, final String zkUserName, final String serviceId, ZKClient zkClient) {
+        clients.put(appendKey(zkAddr, zkUserName, serviceId), zkClient);
     }
 
     public boolean exist(final String zkAddr, final String zkUserName) {
         return clients.containsKey(appendKey(zkAddr, zkUserName));
     }
-    
-    public boolean exist(final String zkAddr, final String zkUserName,final String serviceId) {
-        return clients.containsKey(appendKey(zkAddr, zkUserName,serviceId));
+
+    public boolean exist(final String zkAddr, final String zkUserName, final String serviceId) {
+        return clients.containsKey(appendKey(zkAddr, zkUserName, serviceId));
     }
 
 }
--- iPaas-Zookeeper-Utility/src/main/java/com/ai/paas/ipaas/ccs/zookeeper/impl/ZKPoolFactory.java
+++ iPaas-Zookeeper-Utility/src/main/java/com/ai/paas/ipaas/ccs/zookeeper/impl/ZKPoolFactory.java
@@ -8,65 +8,25 @@ import com.ai.paas.ipaas.util.Assert;
 import com.ai.paas.ipaas.util.ResourceUtil;
 import com.ai.paas.ipaas.util.StringUtil;
 
-public class ZKPoolFactory
-{
+public class ZKPoolFactory {
     private static final ZKPool zkPool = new ZKPool();
 
     public static ZKPool getZKPool(String zkAddress, String zkUser, String zkPasswd, int timeOut) throws ConfigException {
         validateParam(zkAddress, zkUser, zkPasswd);
-        if (zkPool.exist(zkAddress, zkUser)) {
-            return zkPool;
-        }
-        ZKClient client = null;
-        try {
-            client = new ZKClient(zkAddress, timeOut, new String[] {ConfigCenterConstants.ZKAuthSchema.DIGEST, getAuthInfo(zkUser, zkPasswd) });
-            client.addAuth(ConfigCenterConstants.ZKAuthSchema.DIGEST, getAuthInfo(zkUser, zkPasswd));
-        } catch (Exception e) {
-            throw new ConfigException(ResourceUtil.getMessage(BundleKeyConstant.GET_CONFIG_CLIENT_FAILED));
-        }
-        zkPool.addZKClient(zkAddress, zkUser, client);
-        return zkPool;
-    }
-    
-    public static ZKPool getZKPool(String zkAddress, String zkUser, String zkPasswd, String serviceId ,int timeOut) throws ConfigException {
-        validateParam(zkAddress, zkUser, zkPasswd ,serviceId);
-        if (zkPool.exist(zkAddress, zkUser ,serviceId)) {
-            return zkPool;
-        }
-        ZKClient client = null;
-        try {
-            client = new ZKClient(zkAddress, timeOut, new String[] {ConfigCenterConstants.ZKAuthSchema.DIGEST, getAuthInfo(zkUser, zkPasswd) });
-            client.addAuth(ConfigCenterConstants.ZKAuthSchema.DIGEST, getAuthInfo(zkUser, zkPasswd));
-        } catch (Exception e) {
-            throw new ConfigException(ResourceUtil.getMessage(BundleKeyConstant.GET_CONFIG_CLIENT_FAILED));
-        }
-        zkPool.addZKClient(zkAddress, zkUser, serviceId,client);
-        return zkPool;
+        return getZkPool0(zkAddress, zkUser, zkPasswd, null, timeOut);
     }
 
-    private static void validateParam(String zkAddress, String zkUser, String zkPasswd) {
-        Assert.notNull(zkAddress, ResourceUtil.getMessage(BundleKeyConstant.CONFIG_ADDRESS_IS_NULL));
-        Assert.notNull(zkUser, ResourceUtil.getMessage(BundleKeyConstant.USER_NAME_IS_NULL));
-        Assert.notNull(zkPasswd, ResourceUtil.getMessage(BundleKeyConstant.PASSWD_IS_NULL));
-    }
-    
-    private static void validateParam(String zkAddress, String zkUser, String zkPasswd,String serviceId) {
-        Assert.notNull(zkAddress, ResourceUtil.getMessage(BundleKeyConstant.CONFIG_ADDRESS_IS_NULL));
-        Assert.notNull(zkUser, ResourceUtil.getMessage(BundleKeyConstant.USER_NAME_IS_NULL));
-        Assert.notNull(zkPasswd, ResourceUtil.getMessage(BundleKeyConstant.PASSWD_IS_NULL));
-        Assert.notNull(serviceId, "service id is null!");
-    }
-
-    private static String getAuthInfo(String zkUser, String zkPasswd) {
-        return zkUser + ":" + zkPasswd;
+    public static ZKPool getZKPool(String zkAddress, String zkUser, String zkPasswd, String serviceId, int timeOut) throws ConfigException {
+        validateParam(zkAddress, zkUser, zkPasswd, serviceId);
+        return getZkPool0(zkAddress, zkUser, zkPasswd, serviceId, timeOut);
     }
 
     public static ZKPool getZKPool(String zkAddress, String zkUser, String zkPasswd) throws ConfigException {
         return getZKPool(zkAddress, zkUser, zkPasswd, 2000);
     }
-    
-    public static ZKPool getZKPool(String zkAddress, String zkUser, String zkPasswd ,String serviceId) throws ConfigException {
-        return getZKPool(zkAddress, zkUser, zkPasswd, serviceId,2000);
+
+    public static ZKPool getZKPool(String zkAddress, String zkUser, String zkPasswd, String serviceId) throws ConfigException {
+        return getZKPool(zkAddress, zkUser, zkPasswd, serviceId, 2000);
     }
 
     public static ZKPool getZKPool(String zkAddress, int timeOut, String... authInfo) throws ConfigException {
@@ -80,6 +40,45 @@ public class ZKPoolFactory
                 zkPasswd = authInfo[1];
             }
         }
-        return getZKPool(zkAddress, zkUser, zkPasswd, 2000);
+        return getZkPool0(zkAddress, zkUser, zkPasswd, null, timeOut);
     }
+
+    private static ZKPool getZkPool0(String zkAddress, String zkUser, String zkPasswd, String serviceId, int timeOut) throws ConfigException {
+        if (zkPool.exist(zkAddress, zkUser, serviceId)) {
+            return zkPool;
+        }
+        ZKClient client = null;
+        try {
+            if (!StringUtil.isBlank(zkUser) && !StringUtil.isBlank(zkPasswd)) {
+                client = new ZKClient(zkAddress, timeOut, new String[]{ConfigCenterConstants.ZKAuthSchema.DIGEST, getAuthInfo(zkUser, zkPasswd)});
+                client.addAuth(ConfigCenterConstants.ZKAuthSchema.DIGEST, getAuthInfo(zkUser, zkPasswd));
+            } else {
+                client = new ZKClient(zkAddress, timeOut, null);
+            }
+        } catch (Exception e) {
+            throw new ConfigException(ResourceUtil.getMessage(BundleKeyConstant.GET_CONFIG_CLIENT_FAILED));
+        }
+        zkPool.addZKClient(zkAddress, zkUser, serviceId, client);
+        return zkPool;
+    }
+
+    private static void validateParam(String zkAddress, String zkUser, String zkPasswd) {
+        Assert.notNull(zkAddress, ResourceUtil.getMessage(BundleKeyConstant.CONFIG_ADDRESS_IS_NULL));
+        Assert.notNull(zkUser, ResourceUtil.getMessage(BundleKeyConstant.USER_NAME_IS_NULL));
+        Assert.notNull(zkPasswd, ResourceUtil.getMessage(BundleKeyConstant.PASSWD_IS_NULL));
+    }
+
+    private static void validateParam(String zkAddress, String zkUser, String zkPasswd, String serviceId) {
+        Assert.notNull(zkAddress, ResourceUtil.getMessage(BundleKeyConstant.CONFIG_ADDRESS_IS_NULL));
+        Assert.notNull(zkUser, ResourceUtil.getMessage(BundleKeyConstant.USER_NAME_IS_NULL));
+        Assert.notNull(zkPasswd, ResourceUtil.getMessage(BundleKeyConstant.PASSWD_IS_NULL));
+        Assert.notNull(serviceId, "service id is null!");
+    }
+
+    private static String getAuthInfo(String zkUser, String zkPasswd) {
+        return zkUser + ":" + zkPasswd;
+    }
+
+
+
 }
